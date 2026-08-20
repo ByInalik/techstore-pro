489f346615617c3437c8ae7bb399a1634a1f2dc4
Inalik <hernandezmaicol106@gmail.com>
2026-08-19 22:07:23 -0500

    GET, POST, PUT, DELETE

--- backend/server.js
+++ backend/server.js
@@ -32,13 +32,49 @@ app.get('/api/productos', async (req, res) => {
     }
 });
 
-//6 . ruta de prueba
+//6. Ruta POST /api/productos - crear un productos nuevo < - AGREGA aqui
+app.post('/api/productos', async (req, res) => {
+    try {
+        const nuevoProducto = await Producto.create(req.body); // toma el JSON del body
+        res.status(201).json(nuevoProducto);                   // 201 = created
+    } catch (err) {
+      res.status(400).json({ error: err.message });            // 400 = datos invalidos
+    }
+});
+
+//7. Ruta PUT /api/productos/:id - actualizar un producto
+app.put('/api/productos/:id', async (req, res) => {
+    try {
+        const actualizado = await Producto.findByIdAndUpdate(
+            req.params.id, // _id de MongoDB que viene en la URL
+            req.body,      // campos nuevos que viene en el body
+            { new: true }  // retorno el documento YA actualizado
+        );
+        if (!actualizado) return res.status(404).json({ error: 'Producto no encontrado' });
+        res.json(actualizado);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+});
+
+//8. Ruta DELETE /api/productos/:id - eliminar un producto
+app.delete('/api/productos/:id', async (req, res) => {
+    try {
+        const eliminado = await Producto.findByIdAndDelete(req.params.id);
+        if (!eliminado) return res.status(404).json({ error: 'Producto no encontrado'});
+        res.json({ mensaje: 'Producto eliminado correctamente', eliminado});
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+});
+
+//9 . ruta de prueba
 
 app.get('/', (req, res) => {
     res.json({ mensaje: 'Servidor TechStore Pro ✅'});
 });
 
-//6 . Arrancar el servidor 
+//10 . Arrancar el servidor 
 
 app.listen(PORT, ()  => {
     console.log(`Servidor en https://localhost:${PORT}`);
