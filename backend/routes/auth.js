7be5252b22848f4186f7fe16758b52b6c71ad1a4
Inalik <hernandezmaicol106@gmail.com>
2026-08-20 22:10:55 -0500

    jwt

---
+++ backend/routes/auth.js
@@ -0,0 +1,55 @@
+//1. Importar dependencias
+const express   = require('express');
+const bcrypt    = require('bcryptjs');
+const jwt       = require('jsonwebtoken');
+const Usuario   = require('../models/Usuario');
+const router    = express.Router();
+
+//2. Post /api/auth/registro - Crear cuenta nueva
+router.post('/registro', async (req, res) => {
+    try {
+        const { nombre, email, password } = req.body;
+
+        // verificar que el email no exista ya
+        const existe = await Usuario.findOne({ email });
+        if (existe) return res.status(400).json({ error: 'El email ya está registrado'});
+
+        //Encriptar la constraseña en 10 rondas de bcrypt
+        const hash = await bcrypt.hash(password, 10);
+
+        // Guardar el usuario con la constraseña encriptada
+        const usuario = await Usuario.create({ nombre, email, password: hash});
+
+        res.status(201).json({ mensaje: 'Usuario creado correctamente', id: usuario._id });
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+});
+
+//3. POST /api/auth/login - iniciar sesión y recibir token
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // Buscar usuario por email
+        const usuario = await Usuario.findOne({ email });
+        if (!usuario) return res.status(401).json({ error: 'Email o contraseña' });
+
+        // comparar la contraseña con el hash guardado en Atlas
+        const valida = await bcrypt.compare(password, usuario.password);
+        if (!valida) return res.status(401).json({ error: 'Email o contraseña incorrectos'});
+
+        // crear el token JWT - dura 24 horas
+        const token = jwt.sign(
+            { id: usuario._id, email: usuario.email },
+            process.env.JWT_SECRET,
+            { expiresIn: '24h'}
+        );
+        res.json({ token, nombre: usuario.nombre });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+ });
+
+ //4. Exportar el router
+ module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 //1 . Importar las dependencias
 
 require('dotenv').config();
-const express  = require('express');
-const cors     = require('cors');
-const mongoose = require('mongoose');
-const Producto = require('./models/Producto');
+const express           = require('express');
+const cors              = require('cors');
+const mongoose          = require('mongoose');
+const Producto          = require('./models/Producto');
+const authRoutes        = require('./routes/auth');
+const verificarToken    = require('./middleware/auth');
 
 //2 . crear la aplicacion y definir el puerto
 
@@ -33,7 +35,7 @@ app.get('/api/productos', async (req, res) => {
 });
 
 //6. Ruta POST /api/productos - crear un productos nuevo < - AGREGA aqui
-app.post('/api/productos', async (req, res) => {
+app.post('/api/productos', verificarToken, async (req, res) => {
     try {
         const nuevoProducto = await Producto.create(req.body); // toma el JSON del body
         res.status(201).json(nuevoProducto);                   // 201 = created
@@ -43,7 +45,7 @@ app.post('/api/productos', async (req, res) => {
 });
 
 //7. Ruta PUT /api/productos/:id - actualizar un producto
-app.put('/api/productos/:id', async (req, res) => {
+app.put('/api/productos/:id', verificarToken, async (req, res) => {
     try {
         const actualizado = await Producto.findByIdAndUpdate(
             req.params.id, // _id de MongoDB que viene en la URL
@@ -58,7 +60,7 @@ app.put('/api/productos/:id', async (req, res) => {
 });
 
 //8. Ruta DELETE /api/productos/:id - eliminar un producto
-app.delete('/api/productos/:id', async (req, res) => {
+app.delete('/api/productos/:id', verificarToken, async (req, res) => {
     try {
         const eliminado = await Producto.findByIdAndDelete(req.params.id);
         if (!eliminado) return res.status(404).json({ error: 'Producto no encontrado'});
@@ -79,3 +81,6 @@ app.get('/', (req, res) => {
 app.listen(PORT, ()  => {
     console.log(`Servidor en https://localhost:${PORT}`);
 });
+
+//11. Rutas de autenticacion <- NUEVO S14
+app.use('/api/auth', authRoutes);
